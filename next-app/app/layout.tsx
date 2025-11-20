1c6d4b60dfc880a212a1e8b6f37769b538f4ca44
Takuya0202 <shimozono.t.24kdgn@gmail.com>
2025-11-20 16:04:12 +0900

    add

--- next-app/app/layout.tsx
+++ next-app/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,7 +14,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <head>
+        <link
+            href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;500;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+      </head>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
--- next-app/app/profile/page.tsx
+++ next-app/app/profile/page.tsx
@@ -1,4 +1,4 @@
-import Logout from "../feature/auth/logout";
+import Logout from "../components/feature/auth/logout";
 
 export default function ProfilePage() {
   return (
