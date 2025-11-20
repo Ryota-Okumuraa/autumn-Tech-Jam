30b150a5b7823c245d3d58cece28d56171760136
katoyuta <katoyuta021962@gmail.com>
2025-11-20 15:36:27 +0900

    変更前コミット

--- next-app/app/api/posts/ranking/route.ts
+++ next-app/app/api/posts/ranking/route.ts
@@ -1,101 +1,60 @@
 import prisma from "@/lib/db";
-import { cookies } from "next/headers";
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { checkLang } from "@/lib/language";
 
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
     const daysParams = searchParams.get("days"); //3日間か、7日間かどちらかのランキングが入る。
-    const languageParams = searchParams.get("languageCode"); //userがどの言語でみているのか送られてくる。ゲストであれば、リクエストは必要だが、登録している人は、現在ログインしているuserから取ってこれる。<-のほうが良い理由は、リクエストで来ると、分岐が多くなるが、userの情報から取ってくると、分岐はいらない
+    const languageParams = searchParams.get("languageCode") || "en"; //userがどの言語でみているのか送られてくる。ゲストであれば、リクエストは必要だが、登録している人は、現在ログインしているuserから取ってこれる。<-のほうが良い理由は、リクエストで来ると、分岐が多くなるが、userの情報から取ってくると、分岐はいらない
     //絞り込んでる箇所で、postテーブルのprofile->languageがクエリと同じもので絞り込む。
-    const allowedLanguages = ["en", "ja", "zh-CN", "zh-TW", "ko"];
-
-    // languageCode が許可された値でなければ null にする
-    const languageCode = allowedLanguages.includes(languageParams || "")
-        ? languageParams
-        : "en";
-
+    // const allowedLanguages = ["en", "ja", "zh-CN", "zh-TW", "ko"];
+    const language = checkLang(languageParams);
     const days = Number(daysParams);
 
-    // const supabase = createRouteHandlerClient({ cookies });
-    // const {
-    //     data: { user },
-    // } = await supabase.auth.getUser();
+    // 今日から7日前
+    const DaysAgo = new Date(); //現在日の取得
+    DaysAgo.setDate(DaysAgo.getDate() - days);
 
-    // if (!user) return Response.json({ error: "Not logged in" }, { status: 401 });
-
-    // const profile = await prisma.profile.findUnique({
-    //     where: { userId: user.id },
-    // });
-
-    if (days === 7) {
-        // 今日から7日前
-        const sevenDaysAgo = new Date(); //現在日の取得
-        sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
-
-        const sevenPosts = await prisma.post.findMany({
-            where: {
-                views: {
-                    some: {
-                        createdAt: {
-                            gte: sevenDaysAgo,
-                        },
-                    },
-                },
-                languageId: language.id,
-            },
-            include: {
-                _count: {
-                    select: {
-                        views: {
-                            where: {
-                                createdAt: {
-                                    gte: sevenDaysAgo,
-                                },
-                            },
-                        },
+    const rankingPosts = await prisma.post.findMany({
+        where: {
+            views: {
+                some: {
+                    createdAt: {
+                        gte: DaysAgo,
                     },
                 },
             },
-            orderBy: {
-                views: {
-                    _count: "desc",
+            language: {
+                name: language,  // ← ココが正解！
+            },
+        },
+        select: {
+            id: true,
+            thumbnail: true,
+            title: true,
+            createdAt: true,
+            category: {
+                select: {
+                    name: true, // ← categoryId は返さず category の名前だけ返す
                 },
             },
-        });
-        return Response.json(sevenPosts);
-    }
-    else if (days === 3) {
-        const threeDaysAgo = new Date();
-        threeDaysAgo.setDate(threeDaysAgo.getDate() - 3);
-
-        const threePosts = await prisma.post.findMany({
-            where: { //3日以内の view が 1件でもついている投稿だけ取得して！
-                views: {
-                    some: {
-                        createdAt: {
-                            gte: threeDaysAgo,
-                        },
-                    },
-                },
+            // _count: {
+            //     select: {
+            //         views: {
+            //             where: {
+            //                 createdAt: {
+            //                     gte: DaysAgo,
+            //                 },
+            //             },
+            //         },
+            //     },
+            // },
+        },
+        orderBy: {
+            views: {
+                _count: "desc",
             },
-            include: { //その投稿に対して、3日以内の views の数をカウントして追加して！
-                _count: {
-                    select: {
-                        views: {
-                            where: {
-                                createdAt: {
-                                    gte: threeDaysAgo,
-                                },
-                            },
-                        },
-                    },
-                },
-            },
-            orderBy: { //閲覧数（views の件数）が多い順に並び替えてください
-                views: {
-                    _count: "desc",
-                },
-            },
-        })
-    }
+        },
+        take: 10,
+    });
+    return Response.json(rankingPosts);
 }
