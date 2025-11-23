3fbafc8514ed78e3ea0391ed8fa0620d5cbb6309
Takuya0202 <shimozono.t.24kdgn@gmail.com>
2025-11-24 03:21:46 +0900

    変更取り込み

---
+++ next-app/app/components/shared/PostCard.tsx
@@ -0,0 +1,40 @@
+import { cn } from "@/lib/utiles";
+import Link from "next/link";
+import Image from "next/image"
+
+type PostCardProps = {
+    id: string;
+    title: string;
+    thumbnail: string;
+    date: string;
+    author: string;
+    layout: 'row' | 'column';
+}
+
+export function PostCard({ title, thumbnail, date, author, layout, id }: PostCardProps) {
+    return (
+        <Link
+            href={`/post/${id}`}
+            className={cn(
+                "min-w-[200px] md:min-w-none",
+                layout === "row" ? "flex max-w-[312px] md:max-w-full " : "max-w-[218px] md:max-w-full",
+            )}>
+            <div className="flex overflow-hidden duration-200 rounded-xl border-2 border-black hover:border-main w-fit h-fit">
+                <Image
+                    src={thumbnail}
+                    alt="thumbnail"
+                    width={400}
+                    height={400}
+                    className="transition-transform ease-in-out object-cover hover:scale-105"
+                />
+            </div>
+            <div className="flex flex-col gap-3">
+                <h4 className="flex line-clamp-2 text-[14px] mt-4 font-bold h- md:text-md">{title}</h4>
+                <div className="flex gap-6 text-xs items-center md:text-[14px]">
+                    <span>{date}</span>
+                    <span>{author}</span>
+                </div>
+            </div>
+        </Link>
+    )
+}
--- next-app/app/components/shared/PostTitleEditor.tsx
+++ next-app/app/components/shared/PostTitleEditor.tsx
@@ -3,27 +3,30 @@
 import { cn } from "@/lib/utiles";
 
 interface PostTitleEditorProps {
-  title: string;
-  setTitle: (title: string) => void;
-  error?: string;
+    title: string;
+    setTitle: (title: string) => void;
+    error?: string;
 }
 
 export const PostTitleEditor = ({ title, setTitle, error }: PostTitleEditorProps) => {
-  return (
-    <div>
-      <label htmlFor="title" className="block ">
-        タイトル
-      </label>
-      <input
-        id="title"
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        className={cn(
-          "w-full px-4 py-3 border border-black rounded-lg bg-white mt-2 focus:outline-main",
-          error && "border-red-500"
-        )}
-      />
-    </div>
-  );
-};
+    return (
+        <div>
+            <label
+                htmlFor="title"
+                className='block '
+            >
+                タイトル
+            </label>
+            <input
+                id='title'
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                className={cn(
+                    'w-full px-4 py-3 border border-black rounded-lg bg-white mt-2 focus:outline-main',
+                    error && 'border-red-500'
+                )}
+            />
+        </div>
+    )
+}
