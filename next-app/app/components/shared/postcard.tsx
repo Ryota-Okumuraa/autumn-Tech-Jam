c4db9b75262109d6d9c1a40a376453920d99c678
Takuya0202 <shimozono.t.24kdgn@gmail.com>
2025-11-25 05:17:32 +0900

    コミット忘れ？？

--- next-app/app/components/shared/postcard.tsx
+++ next-app/app/components/shared/postcard.tsx
@@ -1,24 +1,52 @@
 import { cn } from "@/lib/utiles";
-import Image from "next/image"
-type Page = {
-    title:string;
-    thumbnail : string;
-    createdAt : string;
-    author : string;
-    layout : 'row' | 'column';
-}
+import { Link } from "@/i18n/routing";
+import Image from "next/image";
+import { PostCardProps } from "@/lib/types/post";
 
-export function PostCard ({title, thumbnail, createdAt, author, layout}:Page) {
-    return (
-        <div className={cn
-            (
-            layout === "row" ? "flex max-w-[312px] " : "max-h-[218px]" ,
-        )}>
-        <div className="flex overflow-hidden duration-200 rounded-xl border-2 border-black hover:border-main w-fit h-fit">
-        <Image src={thumbnail} alt="thumbnail" width={272} height={172} className="transition-transform ease-in-out object-cover
-        hover:scale-105"/>
+export function PostCard({ title, thumbnail, date, author, layout, id }: PostCardProps) {
+  return (
+    <Link
+      href={`/post/${id}`}
+      className={cn(
+        "min-w-[200px] md:min-w-none flex",
+        layout === "row"
+          ? "max-w-[314px] md:max-w-full gap-4 md:gap-6 "
+          : "max-w-[218px] md:max-w-[280px] w-full flex-col md:min-w-[280px]"
+      )}
+    >
+      <div
+        className={cn(
+          "flex overflow-hidden duration-200 rounded-xl border-2 border-black hover:border-main w-fit ",
+          layout === "row" && "flex-1"
+        )}
+      >
+        <Image
+          src={thumbnail}
+          alt="thumbnail"
+          width={400}
+          height={400}
+          className="transition-transform ease-in-out object-cover hover:scale-105"
+        />
+      </div>
+      <div
+        className={cn(
+          "flex flex-col gap-3 flex-1",
+          layout === "row" && "items-start justify-center text-left"
+        )}
+      >
+        <h4
+          className={cn(
+            "flex line-clamp-2 text-[14px] mt-4 font-bold h- md:text-[16px]",
+            layout === "row" && "mt-0"
+          )}
+        >
+          {title}
+        </h4>
+        <div className="flex gap-6 text-xs items-center md:text-[14px]">
+          <span>{date}</span>
+          <span>{author}</span>
         </div>
-        <div className="flex p-2 font-bold">{title}{createdAt}{author}</div>
-        </div>
-    )
+      </div>
+    </Link>
+  );
 }
